f7b454bce7e700c00596b1a481ac354dcba96c5b
sophiaorlando <sophia19orlando@gmail.com>
2020-11-18 17:17:24 -0500

    updated files

---
+++ client/build/precache-manifest.a40ef8bee37e5527546fee5ac6253578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8a0887b0b757c749d3bf8aaf29c4b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada7671b1f90699a3e26",
+    "url": "/static/css/main.38d7648b.chunk.css"
+  },
+  {
+    "revision": "643433ba0a3fbb1752ea",
+    "url": "/static/js/2.e2054783.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.e2054783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada7671b1f90699a3e26",
+    "url": "/static/js/main.225ce664.chunk.js"
+  },
+  {
+    "revision": "2b8f580612459d80dc88",
+    "url": "/static/js/runtime-main.f65105ae.js"
+  }
+]);
